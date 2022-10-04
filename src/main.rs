dfe7e98ffc1f32ef8d5a46d6cdbd1f8974eaf648
Cyprien Borée <boreec@tuta.io>
2022-10-04 17:55:11 +0900

    put game_loop outside the main function

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use sdl2::keyboard::Keycode;
 const WINDOW_WIDTH: u32 = 800;
 const WINDOW_HEIGHT: u32 = 600;
 const WINDOW_TITLE: &str = "pong";
+
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -18,10 +19,16 @@ pub fn main() {
 
     let mut canvas = window.into_canvas().build().unwrap();
 
+    game_loop(&sdl_context, &mut canvas);
+
+}
+
+fn game_loop(context: &sdl2::Sdl,
+             canvas: &mut sdl2::render::Canvas<sdl2::video::Window>) {
     canvas.set_draw_color(Color::BLACK);
     canvas.clear();
 
-    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut event_pump = context.event_pump().unwrap();
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
