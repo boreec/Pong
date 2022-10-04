fa4a40d014cc873498fe2e381032ab8a29925c6b
Cyprien Borée <boreec@tuta.io>
2022-10-04 17:48:09 +0900

    set window settings

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,14 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
 
+const WINDOW_WIDTH: u32 = 800;
+const WINDOW_HEIGHT: u32 = 600;
+const WINDOW_TITLE: &str = "pong";
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    let window = video_subsystem.window(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT)
         .position_centered()
         .build()
         .unwrap();
