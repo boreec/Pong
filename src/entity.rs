93c62b642557f0bf684c4f4112c48e0d76e1ebf9
Cyprien Borée <boreec@tuta.io>
2022-10-05 14:40:06 +0900

    add speed parameter to Ball

--- src/entity.rs
+++ src/entity.rs
@@ -18,14 +18,15 @@ pub struct Ball {
     pub pos_x: i32,
     pub pos_y: i32,
     pub radius: i32,
-    pub color: sdl2::pixels::Color,
     pub direction: Vector2D<i32>,
+    pub speed: i32, // the number of pixels/frame
+    pub color: sdl2::pixels::Color,
 }
 
 impl Ball {
     pub fn update_position(&mut self) {
-        self.pos_x += self.direction.x;
-        self.pos_y += self.direction.y;
+        self.pos_x = self.pos_x + self.speed * self.direction.x;
+        self.pos_y = self.pos_y + self.speed * self.direction.y;
     }
 }
 
@@ -44,6 +45,7 @@ pub fn initialize_game_state() -> GameState {
             WINDOW_HEIGHT as i32 / 2,
             10,
             Vector2D::new(1, 0),
+            10,
             Color::RGB(255,140,0),
         ),
         racket_1: initialize_racket(
@@ -74,12 +76,13 @@ pub fn initialize_racket(x: i32, y: i32, h: u32, w: u32, c: Color) -> Racket {
     };
 }
 
-pub fn initialize_ball(x: i32, y: i32, r: i32, d: Vector2D<i32>, c: Color) -> Ball {
+pub fn initialize_ball(x: i32, y: i32, r: i32, d: Vector2D<i32>, s: i32, c: Color) -> Ball {
     return Ball {
         pos_x: x,
         pos_y: y,
         radius: r,
         direction: d,
+        speed: s,
         color: c,
     };
 }
